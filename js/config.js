a3e21dd8f271307efb0c049fa45f62a59be041fc
jajun-transport <webjajun@gmail.com>
2026-02-04 21:37:28 +0700

    v2

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ export const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbxm7j_An_2
 
 // Slider (biarkan ini sama seperti versi kamu)
 export const SLIDES = [
-  { src: "https://drive.google.com/uc?export=view&id=1XK_dhQ3SvS4jJ61VfcCqK99T3t_LFCRv", label: "Mobil • nyaman untuk keluarga & event" },
-  { src: "https://drive.google.com/uc?export=view&id=1-Y6vZWP9PzFHd2NVvJALFfet5TnS-MFg", label: "Motor • siap harian & perjalanan" },
-  { src: "https://drive.google.com/uc?export=view&id=1MhKytlT3sMbbu8XeSKQKTngohVW17_X4", label: "Booking cepat • via WhatsApp" }
+  { src: "https://drive.google.com/uc?export=view&id=1bNKuy-DimjbnDMxbuQlJ2iyNCsOKl891", label: "Mobil • nyaman untuk keluarga & event" },
+  { src: "https://drive.google.com/uc?export=view&id=1doA5pTfH2XNaCnRbyHAbkRTKoHltEmS2", label: "Motor • siap harian & perjalanan" },
+  { src: "https://drive.google.com/uc?export=view&id=11JYEbaSuAjqqRbZQC4bDorWHldfZJxV8", label: "Booking cepat • via WhatsApp" }
 ];
